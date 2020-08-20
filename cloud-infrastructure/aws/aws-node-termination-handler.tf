ef9150c037ab89ffa611602074ff666182d8d8ef
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-08-20 05:13:54 -0600

    Remove git submodule (#5)

---
+++ cloud-infrastructure/aws/aws-node-termination-handler.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "aws-node-termination-handler" {
+  name       = "aws-node-termination-handler"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-node-termination-handler"
+
+  set {
+    name  = "serviceAccount.name"
+    value = "iamserviceaccount-${var.name_prefix}aws-node-termination-handler"
+  }
+}
---
+++ cloud-infrastructure/aws/mount_volume.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "Running start-up script as root"
+
+# Auto-Mount EFS Drive
+yum install -y amazon-efs-utils
+MOUNTPOINT=/mnt/efs
+mkdir $MOUNTPOINT
+
+sudo mount -t efs fs-cea205cb:/ $MOUNTPOINT
+echo "Ephemeral disk mounted to $MOUNTPOINT"
